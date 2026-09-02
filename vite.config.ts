65403f5a23503f0ddee3f99614c0109cac140e5b
Vic <Vic.Chen@bigbeartech.io>
2026-09-02 12:58:56 +0800

    fix: base url error

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/quiz-game-frontend-manager/" : "/",
+  base: mode === "production" ? "/quiz-game-frontend-client/" : "/",
 }));
